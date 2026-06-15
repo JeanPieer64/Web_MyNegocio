049e76a594f957362ea9e5abcc4a49b2548f65c8
JeanPieer64 <jeancaqui64@gmail.com>
2026-06-14 19:30:18 -0500

    Update db.js

--- src/models/db.js
+++ src/models/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '', // Tu clave local aquí (si tienes)
-    database: process.env.DB_NAME || 'mynegocio', // Tu base de datos local
-    port: process.env.DB_PORT || 3306
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
