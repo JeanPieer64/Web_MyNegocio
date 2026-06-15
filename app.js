c63c5c7cd9aca11c4244d52a5d65884d05266c05
JeanPieer64 <jeancaqui64@gmail.com>
2026-06-14 19:48:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./src/models/db');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // ==========================================
 // 1. CONFIGURACIONES
